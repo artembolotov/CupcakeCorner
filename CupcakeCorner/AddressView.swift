6e832eabec0687ba16f6bfbd19ff0e1595d8a00f
Artem Bolotov <teeemon@gmail.com>
2023-02-15 08:25:27 +0300

    Implemented by tutorial

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,41 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by artembolotov on 13.02.2023.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @ObservedObject var order: Order
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Check out")
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AddressView(order: Order())
+        }
+    }
+}
---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,79 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by artembolotov on 13.02.2023.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    @State private var confirmationMessage: String = ""
+    @State private var showingConfirmation: Bool = false
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("Your total cost is: \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                
+                Button("Place order") {
+                    Task { await placeOrder() }
+                }
+                .padding()
+            }
+        }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you", isPresented: $showingConfirmation) {
+            Button("OK") {
+                
+            }
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x\(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+        }
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            CheckoutView(order: Order())
+        }
+    }
+}
