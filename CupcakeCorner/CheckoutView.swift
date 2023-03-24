7fc5d5e325f68b377771ffad4da4d40c3b31dd20
Artem Bolotov <teeemon@gmail.com>
2023-03-24 09:07:11 +0300

    Hide image and loading indicator from accessibility

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -24,6 +24,7 @@ struct CheckoutView: View {
                     ProgressView()
                 }
                 .frame(height: 233)
+                .accessibilityHidden(true)
                 
                 Text("Your total cost is: \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
